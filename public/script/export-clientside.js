243ab0f0dff25b317119b1febc69723bcfb22df8
PradeepGowdaH <pradeephgowda.pg@gmail.com>
2024-04-04 11:44:02 +0530

    Import and export transactions completed

---
+++ public/script/export-clientside.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const exportButton = document.querySelector(".export-button");
+  const panNumberInput = document.getElementById("panNumber");
+
+  exportButton.addEventListener("click", () => {
+    const panNumber = panNumberInput.value; // Dynamically get the PAN number
+    if (!panNumber) {
+      alert("PAN Number is required");
+      return;
+    }
+    window.location.href = `/export-transactions?panNumber=${panNumber}`;
+  });
+});
+
+// ABCDY1234F
